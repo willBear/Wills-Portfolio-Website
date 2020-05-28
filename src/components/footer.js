1930b580ddd56a28a68096ed2985dd17fc68a2dd
Will Xiong <will_xiong@hotmail.com>
2020-05-28 15:19:53 -0700

    Revert

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  return (
+    <footer>
+      <a href='https://github.com/appjitsu/jasonleecochran_com' target='_blank'><img className='fork' src={`${process.env.PUBLIC_URL}/img/fork.png`} alt='' /></a>
+      <img className='logos' src={`${process.env.PUBLIC_URL}/img/logos.png`} alt='' />
+      <p className="copyright">&copy; {year} willXiong.com. All rights reserved.</p>
+  	</footer>
+  )
+}
+
+export default Footer;
